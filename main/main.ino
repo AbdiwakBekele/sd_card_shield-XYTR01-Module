0404094fe243d8908587fce0fa7bf05792e11dac
AbdiwakBekele <abdiwakbek3226@gmail.com>
2023-11-17 11:38:54 +0300

    Updated SD card Code

--- main/main.ino
+++ main/main.ino
@@ -1,66 +1,88 @@
 #include <SPI.h>
 #include <SD.h>
 
-const int chipSelect = 10;
-const int baudRate = 9600;
-
 File dataFile;
 
 void setup() {
-  Serial.begin(baudRate);
+  Serial.begin(9600);
+  Serial.println("Initializing SD..");
 
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
+  pinMode(10, OUTPUT);
+
+  if (!SD.begin(4)) {
+    Serial.println("Initialization failed!");
     return;
   } else {
-    Serial.println("Card Present");
+    Serial.println("Initilization Done");
+    delay(500);
+    dataFile = SD.open("sensor.csv", FILE_WRITE);
+    if (dataFile) {
+      Serial.println("Writing to SD");
+      dataFile.print("Date Time");
+      dataFile.print(",");
+      dataFile.print("Temprature"); // Adjust the number of decimal places as needed
+      dataFile.print(",");
+      dataFile.println("Humidity");
+  
+      dataFile.flush();
+    } else {
+      Serial.println("Failed to write to SdCrar");
+    }
   }
-
-  dataFile = SD.open("sensorData.csv", FILE_WRITE);
-  if (!dataFile) {
-    Serial.println("Failed to create data file");
-    return;
-  }
-
-  dataFile.println("Date,Time,Temperature,Humidity");
 }
+
 
 void loop() {
-  //  if (Serial.available() > 0) {
-  //    String data = Serial.readStringUntil('\n'); // Read until newline character
-  //
-  //    // Find the positions of commas in the string
-  //    int firstComma = data.indexOf(',');
-  //    int secondComma = data.indexOf(',', firstComma + 1);
-  //    int thirdComma = data.indexOf(',', secondComma + 1);
-  //
-  //    // Extract substrings based on comma positions
-  //    String date = data.substring(0, firstComma);
-  //    String time = data.substring(firstComma + 1, secondComma);
-  //    String temperatureStr = data.substring(secondComma + 1, thirdComma);
-  //    String humidityStr = data.substring(thirdComma + 1);
-  //
-  //    // Convert temperature and humidity to float
-  //    float temperature = temperatureStr.toFloat();
-  //    float humidity = humidityStr.toFloat();
-  //
-  //    // Write data to the CSV file
-  //    dataFile.print(date);
-  //    dataFile.print(",");
-  //    dataFile.print(time);
-  //    dataFile.print(",");
-  //    dataFile.print(temperature, 2); // Adjust the number of decimal places as needed
-  //    dataFile.print(",");
-  //    dataFile.println(humidity, 2);
-  //
-  //    dataFile.flush();
-  //  }
-  //
-  //  // Add a delay to avoid reading too fast and to allow time for other operations
-  //  delay(100);
+
+  unsigned long currentMillis = millis();
+
+  // Calculate hours, minutes, and seconds
+  unsigned long seconds = currentMillis / 1000;
+  unsigned long minutes = seconds / 60;
+  unsigned long hours = minutes / 60;
+
+  // Extract individual components
+  int secondsValue = seconds % 60;
+  int minutesValue = minutes % 60;
+  int hoursValue = hours % 24;
+
+  String timeString = String(hoursValue) + ":" + String(minutesValue) + ":" + String(secondsValue);
+
+  
+    if (Serial.available() > 0) {
+      String data = Serial.readStringUntil('\n'); // Read until newline character
+  
+      // Find the positions of commas in the string
+      int firstComma = data.indexOf(',');
+  
+      // Extract substrings based on comma positions
+      String temperatureStr = data.substring(0, firstComma);
+      String humidityStr = data.substring(firstComma + 1);
+  
+      // Convert temperature and humidity to float
+      float temperature = temperatureStr.toFloat();
+      float humidity = humidityStr.toFloat();
+
+      Serial.print(timeString);
+      Serial.print(",");
+      Serial.print(temperature);
+      Serial.print(",");
+      Serial.println(humidity);
+  
+      // Write data to the CSV file
+      dataFile.print(timeString);
+      dataFile.print(",");
+      dataFile.print(temperature, 2); // Adjust the number of decimal places as needed
+      dataFile.print(",");
+      dataFile.println(humidity, 2);
+  
+      dataFile.flush();
+
+      // Add a delay to avoid reading too fast and to allow time for other operations
+    delay(60000);
+    }
+  
+    
 }
 
-//void closeFile() {
-//  dataFile.close();
-//}
 
